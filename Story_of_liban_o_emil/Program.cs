f160d4eee4741d84e4d444972d3c7860cd8ddbf4
Emil Edin <emil_edin@hotmail.com>
2016-04-07 01:17:09 +0200

    inte nött

--- Story_of_liban_o_emil/Program.cs
+++ Story_of_liban_o_emil/Program.cs
@@ -11,8 +11,42 @@ namespace Story_of_liban_o_emil
         static void Main(string[] args)
 
         {
-            Console.WriteLine("if i see this it works!");
+            Boolean harNyckel = false;
+            string input;
+
+            Console.WriteLine("---Välkommen till Karl-Axels Command Line Adventure---");
+            Console.WriteLine("\nDu står i ett tomt rum. Detta enda som finns är en dörr framför dig. Det ligger en nyckel på golvet");
+
+            while (true) //oändlig loop
+            {
+                Console.Write("\n>");
+                input = Console.ReadLine();
+
+                if (input == "dörr" || input == "öppna dörr" || input == "gå till dörr")
+                {
+                    if (harNyckel)
+                    {
+                        Console.WriteLine("Du öppnar dörren och går igenom");
+                        break;
+                    }
+                    else
+                        Console.WriteLine("Den är låst");
+                }
+
+                else if (input == "nyckel" || input == "ta nyckel")
+                {
+                    Console.WriteLine("Du tog nyckeln");
+                    harNyckel = true;
+                }
+                else
+                    Console.WriteLine("Icke gilltigt eller felstavat commando!");
+
+            }
+
+            Console.WriteLine("\nGrattis du gick ut genom dörren!");
             Console.ReadLine();
+
+
         }
     }
 }
