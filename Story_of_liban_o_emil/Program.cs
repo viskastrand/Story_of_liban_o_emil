8545e6fb5547d3d4902adfff990689177bf04934
Emil Edin <emil_edin@hotmail.com>
2016-04-06 09:16:49 +0200

    first commit

---
+++ Story_of_liban_o_emil/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Story_of_liban_o_emil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
